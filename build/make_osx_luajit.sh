2eb318212351681dea0fd6c4df09aa6d0b691f8b
whitecustome <1003781706@qq.com>
2025-06-11 17:43:40 +0800

    Update make_osx_luajit.sh

--- build/make_osx_luajit.sh
+++ build/make_osx_luajit.sh
@@ -4,7 +4,8 @@ cd ..
 cmake --build build_lj_osx --config Release
 # 打印 build_lj_osx 目录的内容 递归显示
 echo "Contents of build_lj_osx directory:"
-ls build_lj_osx -R
+ls -R build_lj_osx
+
 
 mkdir -p plugin_luajit/Plugins/xlua.bundle/Contents/MacOS/
 cp build_lj_osx/Release/xlua.bundle/Contents/MacOS/xlua plugin_luajit/Plugins/xlua.bundle/Contents/MacOS/xlua
