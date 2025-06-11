22fa21c91038d3c264067729d8b658319c27c292
whitecustome <1003781706@qq.com>
2025-06-11 14:10:26 +0800

    编译luajit

--- build/make_android_luajit_arm64.sh
+++ build/make_android_luajit_arm64.sh
@@ -1,10 +1,7 @@
-#if [ -z "$ANDROID_NDK" ]; then
-    export ANDROID_NDK=~/android-ndk-r15c
-#fi
+
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SRCDIR=$DIR/luajit-2.1.0b3
-# ANDROID_NDK=~/android-ndk-r10e
 
 OS=`uname -s`
 PREBUILT_PLATFORM=linux-x86_64
@@ -14,19 +11,20 @@ fi
 
 NDKABI=21
 
-
+ANDROID_NDK=$ANDROID_NDK_HOME
 echo "Building arm64-v8a lib"
-NDKVER=$ANDROID_NDK/toolchains/aarch64-linux-android-4.9
-NDKP=$NDKVER/prebuilt/$PREBUILT_PLATFORM/bin/aarch64-linux-android-
+NDKVER=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64
+NDKP=$NDKVER/bin/
+NDKCC=$NDKP/aarch64-linux-android$NDKABI-clang
+NDKCROSS=$NDKP/aarch64-linux-android-
 NDKARCH="-DLJ_ABI_SOFTFP=0 -DLJ_ARCH_HASFPU=1 -DLUAJIT_ENABLE_GC64=1"  
-NDKF="--sysroot $ANDROID_NDK/platforms/android-$NDKABI/arch-arm64"
 cd "$SRCDIR"
 make clean
-make HOST_CC="gcc -m64" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARCH"
+make HOST_CC="clang -m64" CROSS=$NDKCROSS STATIC_CC=$NDKCC DYNAMIC_CC="$NDKCC -fPIC -O3" TARGET_LD=$NDKCC TARGET_AR="$NDKP/llvm-ar rcus" TARGET_STRIP="$NDKP/llvm-strip" TARGET_SYS=Linux TARGET_FLAGS="--sysroot $ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot $NDKARCH"
 
 cd "$DIR"
 mkdir -p build_lj_v8a && cd build_lj_v8a
-cmake -DUSING_LUAJIT=ON -DANDROID_ABI=arm64-v8a -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang -DANDROID_NATIVE_API_LEVEL=android-21 ../
+cmake -DUSING_LUAJIT=ON -DANDROID_ABI=arm64-v8a -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=aarch64-linux-android-4.9 -DANDROID_NATIVE_API_LEVEL=android-$NDKABI ../
 cd "$DIR"
 cmake --build build_lj_v8a --config Release
 mkdir -p plugin_luajit/Plugins/Android/libs/arm64-v8a/
@@ -34,33 +32,35 @@ cp build_lj_v8a/libxlua.so plugin_luajit/Plugins/Android/libs/arm64-v8a/libxlua.
 
 
 echo "Building armv7 lib"
-NDKVER=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9
-NDKP=$NDKVER/prebuilt/$PREBUILT_PLATFORM/bin/arm-linux-androideabi-
+NDKVER=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64
+NDKP=$NDKVER/bin/
+NDKCC=$NDKP/armv7a-linux-androideabi$NDKABI-clang
+NDKCROSS=$NDKP/armv7a-linux-androideabi-
 NDKARCH="-march=armv7-a -mfloat-abi=softfp -Wl,--fix-cortex-a8"
-NDKF="--sysroot $ANDROID_NDK/platforms/android-$NDKABI/arch-arm"
 cd "$SRCDIR"
 make clean
-make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARCH"
+make HOST_CC="clang -m32" CROSS=$NDKCROSS STATIC_CC=$NDKCC DYNAMIC_CC="$NDKCC -fPIC -O3" TARGET_LD=$NDKCC TARGET_AR="$NDKP/llvm-ar rcus" TARGET_STRIP="$NDKP/llvm-strip" TARGET_SYS=Linux TARGET_FLAGS="--sysroot $ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot $NDKARCH"
 
 cd "$DIR"
 mkdir -p build_lj_v7a && cd build_lj_v7a
-cmake -DUSING_LUAJIT=ON -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.9 -DANDROID_NATIVE_API_LEVEL=android-21 ../
+cmake -DUSING_LUAJIT=ON -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.9 -DANDROID_NATIVE_API_LEVEL=android-$NDKABI ../
 cd "$DIR"
 cmake --build build_lj_v7a --config Release
 mkdir -p plugin_luajit/Plugins/Android/libs/armeabi-v7a/
 cp build_lj_v7a/libxlua.so plugin_luajit/Plugins/Android/libs/armeabi-v7a/libxlua.so
 
 echo "Building x86 lib"
-NDKVER=$ANDROID_NDK/toolchains/x86-4.9
-NDKP=$NDKVER/prebuilt/$PREBUILT_PLATFORM/bin/i686-linux-android-
+NDKP=$NDKVER/bin/
+NDKCC=$NDKP/i686-linux-androideabi$NDKABI-clang
+NDKCROSS=$NDKP/i686-linux-androideabi-
 NDKF="--sysroot $ANDROID_NDK/platforms/android-$NDKABI/arch-x86"
 cd "$SRCDIR"
 make clean
-make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF"
+make HOST_CC="gcc -m32" CROSS=$NDKCROSS STATIC_CC=$NDKCC DYNAMIC_CC="$NDKCC -fPIC -O3" TARGET_LD=$NDKCC TARGET_AR="$NDKP/llvm-ar rcus" TARGET_STRIP="$NDKP/llvm-strip" TARGET_SYS=Linux TARGET_FLAGS="--sysroot $ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
 
 cd "$DIR"
 mkdir -p build_lj_x86 && cd build_lj_x86
-cmake -DUSING_LUAJIT=ON -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=x86-clang -DANDROID_NATIVE_API_LEVEL=android-21 ../
+cmake -DUSING_LUAJIT=ON -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=x86-4.9 -DANDROID_NATIVE_API_LEVEL=android-21 ../
 cd "$DIR"
 cmake --build build_lj_x86 --config Release
 mkdir -p plugin_luajit/Plugins/Android/libs/x86/
