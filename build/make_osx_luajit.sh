f3167a2dfef5cfa35a8ee2f2107d836fde9cd8f3
xiangchen02 <18351967270@163.com>
2025-06-12 11:01:31 +0800

    Update make_osx_luajit.sh

--- build/make_osx_luajit.sh
+++ build/make_osx_luajit.sh
@@ -12,6 +12,7 @@ cmake -DUSING_LUAJIT=ON  -GXcode ../
 cd ..
 cmake --build build_lj_osx --config Release
 
-mkdir -p plugin_luajit/Plugins/xlua.bundle/Contents/MacOS/
-cp build_lj_osx/Release/xlua.bundle plugin_luajit/Plugins/xlua.bundle
+# mkdir -p plugin_luajit/Plugins/xlua.bundle/Contents/MacOS/
+rm -rf plugin_luajit/Plugins/xlua.bundle
+cp -r build_lj_osx/Release/xlua.bundle plugin_luajit/Plugins/xlua.bundle
 
