fbe1d4c4644d68700a994b6a5565ea879e43b5b5
xiangchen02 <18351967270@163.com>
2025-06-12 10:53:48 +0800

    Update make_osx_luajit.sh

--- build/make_osx_luajit.sh
+++ build/make_osx_luajit.sh
@@ -1,3 +1,11 @@
+echo "编译 LuaJIT..."
+cd luajit-2.1.0b3
+
+make clean
+make MACOSX_DEPLOYMENT_TARGET=13.4
+make && sudo make install
+
+cd ..
 
 mkdir -p build_lj_osx && cd build_lj_osx
 cmake -DUSING_LUAJIT=ON  -GXcode ../
